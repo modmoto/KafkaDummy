393c338a6c17fdb58c9e55b1314104f4824f1f01
modmoto <simonheiss87@gmail.com>
2023-04-29 18:23:03 +0200

    first working example

---
+++ KafkaReceiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaReceiver;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var conf = new ConsumerConfig
+        { 
+            GroupId = "test-consumer-group",
+            BootstrapServers = "65.21.139.246:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            c.Subscribe("my-topic");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+                        Console.WriteLine($"message: '{cr.Value}'");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                c.Close();
+            }
+        }
+    }
+}
---
+++ KafkaSender/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaSender;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var conf = new ProducerConfig { BootstrapServers = "65.21.139.246:9092" };
+
+        Action<DeliveryReport<Null, string>> handler = r =>
+        {
+            if (r.Error.IsError)
+            {
+                Console.WriteLine($"Delivery Error: {r.Error.Reason}");
+            }
+        };
+        using (var p = new ProducerBuilder<Null, string>(conf).Build())
+        {
+            while (true)
+            {
+                Console.WriteLine("Nachricht eingeben");
+                var readLine = Console.ReadLine();
+                p.Produce("my-topic", new Message<Null, string> { Value = readLine }, handler);
+                p.Flush();
+            }
+        }
+    }
+}
