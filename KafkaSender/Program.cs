110f310f7e4292009cda4fd6b3f03d944bab5c38
modmoto <simonheiss87@gmail.com>
2023-12-13 14:39:45 +0100

    first examples donzo

--- KafkaSender/Program.cs
+++ KafkaSender/Program.cs
@@ -1,4 +1,5 @@
-﻿using Confluent.Kafka;
+﻿using System.Text;
+using Confluent.Kafka;
 
 namespace KafkaSender;
 
@@ -6,27 +7,43 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var conf = new ProducerConfig { BootstrapServers = "127.0.0.1:9092", AllowAutoCreateTopics = true };
+        var conf = new ProducerConfig
+        {
+            BootstrapServers = "pkc-75m1o.europe-west3.gcp.confluent.cloud:9092",
+            SaslPassword = "E5AL3BwH3tvuz7nnZyc4T/ENN2TC0UUNTOces8gPefP2jtL+G5HRE8hjgI1bpFgJ",
+            SaslUsername = "MTHMXNOJJOMJDWKC",
+            SaslMechanism = SaslMechanism.Plain,
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            Partitioner = Partitioner.Consistent,
+        };
 
-        Action<DeliveryReport<Null, string>> handler = r =>
+        Action<DeliveryReport<string, string>> handler = r =>
         {
             if (r.Error.IsError)
             {
                 Console.WriteLine($"Delivery Error: {r.Error.Reason}");
             }
+            else
+            {
+                Console.WriteLine($"Written to offset: {r.Offset}");
+            }
         };
         
-        using (var p = new ProducerBuilder<Null, string>(conf).Build())
+        using (var p = new ProducerBuilder<string, string>(conf).Build())
         {
-            var partition = 0;
             while (true)
             {
                 Console.WriteLine("Nachricht eingeben");
-                var readLine = $"{partition}: {Console.ReadLine()}";
-                // var topicPartition = new TopicPartition("my-topic", new Partition(partition % 2));
-                // p.Produce(topicPartition, new Message<Null, string> { Value = readLine }, handler);
-                // partition++;
-                p.Produce("my-topic", new Message<Null, string> { Value = readLine }, handler);
+                string simonClient = "Simon-Client";
+                p.Produce(new TopicPartition("SimonHEs-amazing-topic", new Partition(2)), new Message<string, string> 
+                { 
+                    Value = Console.ReadLine(), 
+                    Key = Guid.NewGuid().ToString(),
+                    Headers = new Headers
+                    {
+                        { "producer-name", Encoding.UTF8.GetBytes(simonClient) }
+                    }
+                }, handler);
                 p.Flush();
             }
         }
