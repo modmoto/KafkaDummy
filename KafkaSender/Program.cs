ae7fb9174667eea074f02c825801efe72d804714
simon <simonheiss87@gmail.com>
2023-12-15 09:31:15 +0100

    broken send but technically okish

--- KafkaSender/Program.cs
+++ KafkaSender/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.Text;
 using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using digital.thinkport;
 
 namespace KafkaSender;
 
@@ -17,35 +21,67 @@ class Program
             Acks = Acks.All // default, erst ack zurück wenn alles repliziert ist
             // Partitioner = Partitioner.Consistent,
         };
+        
+        var schemaRegistryConfig = new SchemaRegistryConfig
+        {
+            BasicAuthUserInfo = "3SYHEWSXNVO7EG3P:9xJ+x9hW2AHEQIaqzBifBMBcgWP1kINcXNRy7+fGIe6xlCOlI1UzjVjXvtHMUenQ",
+            BasicAuthCredentialsSource = AuthCredentialsSource.UserInfo,
+            Url = "https://psrc-2312y.europe-west3.gcp.confluent.cloud"
+        };
 
-        Action<DeliveryReport<string, string>> handler = r =>
+        Action<DeliveryReport<string, OrderedPresentChecked>> handler = r =>
         {
             if (r.Error.IsError)
             {
                 Console.WriteLine($"Delivery Error: {r.Error.Reason}");
             }
-            // else
-            // {
-            //     Console.WriteLine($"Written to offset: {r.Offset}");
-            // }
-        };
-        
-        using (var p = new ProducerBuilder<string, string>(conf).Build())
-        {
-            while (true)
+            else
             {
-                var simonClient = "Simon-Client";
-                p.Produce("factory.presents.checked.0", new Message<string, string> 
-                { 
-                    Value = Console.ReadLine(), 
-                    Key = Guid.NewGuid().ToString(),
-                    Headers = new Headers
-                    {
-                        { "producer-name", Encoding.UTF8.GetBytes(simonClient) }
-                    }
-                }, handler);
-                p.Flush();
+                Console.WriteLine($"Written to offset: {r.Offset}");
             }
+        };
+
+        using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        using var p = new ProducerBuilder<string, OrderedPresentChecked>(conf)
+            .SetValueSerializer(new AvroSerializer<OrderedPresentChecked>(schemaRegistry).AsSyncOverAsync())
+            .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+            .Build();
+        while (true)
+        {
+            var simonClient = "Simon-Client";
+            p.Produce("factory.presents.checked.0", new Message<string, OrderedPresentChecked> 
+            { 
+                Value = new OrderedPresentChecked
+                {
+                    brand = "siemons",
+                    checkedAt = 123512,
+                    checkedBy = "simon he",
+                    price = 14.56,
+                    product = Console.ReadLine()
+                }, 
+                Key = Guid.NewGuid().ToString(),
+                Headers = new Headers
+                {
+                    { "producer-name", Encoding.UTF8.GetBytes(simonClient) }
+                }
+            }, handler);
+            // p.ProduceAsync("factory.presents.checked.0", new Message<string, OrderedPresentChecked> 
+            // { 
+            //     Value = new OrderedPresentChecked
+            //     {
+            //         brand = "siemons",
+            //         checkedAt = 123512,
+            //         checkedBy = "simon he",
+            //         price = 14.56,
+            //         product = Console.ReadLine()
+            //     }, 
+            //     Key = Guid.NewGuid().ToString(),
+            //     Headers = new Headers
+            //     {
+            //         { "producer-name", Encoding.UTF8.GetBytes(simonClient) }
+            //     }
+            // }).ContinueWith(task => handler.Invoke(task.));
+            p.Flush();
         }
     }
 }
